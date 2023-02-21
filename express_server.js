ca2171ee44d795297702c09cda447d2bba5d21ff
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-20 21:11:19 -0500

    Completed  URL shortening part 2
    Add GET route to expressserver.js and render using accompanying template
    Add POST route to expressserver.js to receive form submission

--- express_server.js
+++ express_server.js
@@ -4,19 +4,41 @@ const PORT = 8081; // default port 8080
 
 app.set("view engine", "ejs");
 
+const generateRandomString = function() {
+  const length = 6;
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+    
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase["b2xVn2"] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
