60adb0112587d163d26923aa4df192203cd5b1ea
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-25 11:05:28 -0500

    final eslint and readability check.

--- express_server.js
+++ express_server.js
@@ -37,8 +37,10 @@ hashUserObjPasswords(users);
 
 //redirection of homepage to login or urls index
 app.get("/", (req, res) => {
+  
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
+  
   if (loggedInUser) {
     res.redirect('/urls');
   } else {
@@ -48,6 +50,7 @@ app.get("/", (req, res) => {
 
 //verifies the cookie to display the user name in every page if logged in
 app.get("/urls", (req, res) => {
+  
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
   const shortUrlFound = urlsForUser(userId, urlDatabase);
@@ -55,25 +58,31 @@ app.get("/urls", (req, res) => {
     user: loggedInUser,
     filteredUrls: shortUrlFound,
   };
+  
   res.render("urls_index", templateVars);
 });
 
 //random SHORTURL generator
 app.post("/urls", (req, res) => {
+  
   let shortURL = generateRandomId();
   const userId = req.session.user_id;
+  
   urlDatabase[shortURL] = {
     longURL: req.body.longURL,
     userID: userId,
   };
+
   res.redirect(`/urls/${shortURL}`);
 });
 
 //creation of new urls
 app.get("/urls/new", (req, res) => {
+  
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
   const templateVars = {
+  
     user: loggedInUser,
   };
   if (loggedInUser) {
@@ -85,6 +94,7 @@ app.get("/urls/new", (req, res) => {
 
 // creation of short URL save page with cookies enables
 app.get("/urls/:shortURL", (req, res) => {
+  
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
 
@@ -92,8 +102,10 @@ app.get("/urls/:shortURL", (req, res) => {
     res.send("Id does not exist in user database!!");
     return;
   }
+  
   const longURL = urlDatabase[req.params.shortURL].longURL;
   const templateVars = {
+  
     user: loggedInUser,
     shortURL: req.params.shortURL,
     longURL: longURL,
@@ -107,14 +119,19 @@ app.get("/urls/:shortURL", (req, res) => {
 
 //short URL database checker
 app.get("/u/:shortURL", (req, res) => {
+  
   const shortURL = req.params.shortURL;
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
   const templateVars = {
+  
     user: loggedInUser,
   };
+  
   if (urlDatabase[shortURL]) {
+    
     const longURL = urlDatabase[req.params.shortURL].longURL;
+    
     res.redirect(longURL);
   } else {
     res.render('error.ejs', templateVars);
@@ -123,9 +140,12 @@ app.get("/u/:shortURL", (req, res) => {
 
 //delete urls post
 app.post("/urls/:shortURL/delete", (req,res) => {
+  
   const shortURL = req.params.shortURL;
   let idMatchedVar = 0;
+  
   idMatchedVar = idMatched(shortURL, users, urlDatabase);
+  
   if (!idMatchedVar) {
     res.send("Sorry, you dont have permission to delete other user's url");
     return;
@@ -136,16 +156,19 @@ app.post("/urls/:shortURL/delete", (req,res) => {
 
 //Update the long URL
 app.post("/urls/:id", (req,res) => {
+  
   const id = req.params.id;
+  
   urlDatabase[id].longURL = req.body.quoteContent;
   res.redirect("/urls");
 });
 
 //Implementing Cookies
 app.post("/login", (req,res) => {
+  
   const { email, password } = req.body;
   const userFoundByEmail = getUserByEmail(email, users);
-  console.log(userFoundByEmail);
+  
   if (!userFoundByEmail) {
     res.status(403).send("User cannot be found");
   } else {
@@ -166,9 +189,11 @@ app.post("/logout", (req,res) => {
 
 //Implement User Registration and Login
 app.get('/login', (req, res) => {
+
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
   const templateVars = {
+  
     user: loggedInUser};
   if (loggedInUser) {
     res.redirect("/urls");
@@ -179,9 +204,11 @@ app.get('/login', (req, res) => {
 
 //register GET render
 app.get('/register',(req,res) => {
+  
   const userId = req.session.user_id;
   const loggedInUser = users[userId];
   const templateVars = {
+  
     user: loggedInUser};
   if (loggedInUser) {
     res.redirect("/urls");
@@ -192,6 +219,7 @@ app.get('/register',(req,res) => {
 
 //Registering new users and checking for Errors
 app.post('/register',(req, res) => {
+  
   const { email, password } = req.body;
   const hashedPassword = hashThePassword(password);
 
@@ -200,11 +228,13 @@ app.post('/register',(req, res) => {
   }
 
   const userFound = getUserByEmail(email, users);
+ 
   if (userFound) {
     res.status(400).send("User already exists!");
   }
 
   const userID = createUser(email, hashedPassword, users);
+  
   req.session.user_id = userID;
   res.redirect("/urls");
 });
